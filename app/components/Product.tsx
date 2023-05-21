b89ed3be1676d6dc89d5cbf7443175dabfb9a5aa
adam <adammarey51@gmail.com>
2023-05-20 20:27:49 -0700

    fixed price type - styled home page

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -4,14 +4,20 @@ import formatPrice from '@/util/PriceFormat';
 import { ProductType } from '@/types/ProductTypes';
 const Product = ({ name, image, price }: ProductType) => {
   return (
-    <div>
-      <Image src={image} alt={name} width={400} height={400} />
-      <h2>
+    <div className="text-gray-700">
+      <Image
+        src={image}
+        alt={name}
+        width={400}
+        height={400}
+        className="w-full h-95 object-cover rounded-lg"
+      />
+      <h1 className="font-medium py-2">
         {name}
+      </h1>
+      <h2 className="text-sm">
+        {price && formatPrice(price)}
       </h2>
-      <p>
-        {formatPrice(price as number)}
-      </p>
     </div>
   );
 };
